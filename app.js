dd1166b0c36e1bf91ff12517ce0254228c1b86c0
Tochukwu Asogwa <Tochukwuasogwa@gmail.com>
2026-04-03 10:17:38 +0100

    deployment

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config(); // Load .env variables into process.env
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json()); // Parse JSON bodies
+app.use(cors("*")); // Enable CORS for all origins
 
 let todos = [
   { id: 1, task: 'Learn Node.js', completed: false },
@@ -11,11 +15,17 @@ let todos = [
 app.get('/todos', (req, res) => {
   res.status(200).json(todos); // Send array as JSON
 });
+app.get('/todo/:id', (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
+  if (!todo) return res.status(404).json({ message: 'Todo not found' });
+  res.status(200).json(todo); // Send array as JSON
+});
 
 // POST New – Create
 app.post('/todos', (req, res) => {
   const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
   todos.push(newTodo);
+  if(!newTodo.task) return res.status(400).json({ error: 'Task is required' });
   res.status(201).json(newTodo); // Echo back
 });
 
@@ -38,13 +48,17 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 app.get('/todos/completed', (req, res) => {
-  const completed = todos.filter((t) => t.completed);
-  res.json(completed); // Custom Read!
+  const completed = todos.filter((t) => t.completed); // Custom Read: filter by completed
+  res.status(200).json(completed); // Custom Read!
+});
+app.get('/todos/active', (req, res) => {
+  const active = todos.filter((t) => !t.completed); // Custom Read: filter by completed
+  res.status(200).json(active); // Custom Read!
 });
 
 app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Server error!' });
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
